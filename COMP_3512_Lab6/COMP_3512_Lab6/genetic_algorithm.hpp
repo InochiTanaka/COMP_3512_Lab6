d77a606fbbdde8a93340465a3480612054c72dc3
InochiTanaka <yunominosencha@gmail.com>
2018-03-15 02:17:55 -0700

    Stack on store data to permutation

--- COMP_3512_Lab6/COMP_3512_Lab6/genetic_algorithm.hpp
+++ COMP_3512_Lab6/COMP_3512_Lab6/genetic_algorithm.hpp
@@ -1,21 +1,39 @@
-#pragma once
+/* Definitions and then prototypes here */
+
+static constexpr int CITIES_IN_TOUR = 20;
+static constexpr int POPULATION_SIZE = 30;
+static constexpr int NUMBER_OF_PARENTS = 2;
+static constexpr int SHUFFLES = 32;
+static constexpr int ASCII_OFFSET = 65;
+static constexpr int ITERATIONS = 1000;
+static constexpr int MAP_BOUNDARY = 1000;
+static constexpr double FITNESS_SCALER = 10000.0;
+static constexpr int PARENT_POOL_SIZE = 5;
+static constexpr double MUTATION_RATE = 0.15;
+static constexpr int NUMBER_OF_ELITES = 1;
+
 #include <vector>
+#include <iostream>
+#include <utility>
+#include <stack>
 
-class Genetic_Algorithm {
-	private:
-		//static constexpr int CITIES_IN_TOUR = 20;
-		//static constexpr int ASCII_OFFSET = 65;
-		//static constexpr int NUMBER_OF_ELITES = 1;
-		//static constexpr int POPULATION_SIZE = 30;
-		//static constexpr int FITNESS_SCALER = 10000.0;
-		//static constexpr int MAP_BOUNDARY = 1000;
+using namespace std;
 
-		//std::vector<Tour> population;
-		//std::vector<Tour> parents;
-		//std::vector<Tour> crosses;
-		//Tour temporary_tour;
-		//std::vector<City> cities_to_visit;
-		//Tour child;
-
-
+/* A city structure contains:
+a) a name
+b) x and y coordinates */
+struct city {
+	char name;
+	int  x_coordinate;
+	int  y_coordinate;
 };
+
+/* A tour structure contains:
+a) a fitness rating
+b) an array of CITIES_IN_TOUR cities */
+struct tour {
+	double fitness;
+	struct stack<city> permutation;
+};
+
+
